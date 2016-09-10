067c583332c2d9168066924a379f780babb603f1
Andrew Kuchev <0coming.soon@gmail.com>
2016-09-10 12:40:29 +0500

    Initial commit

---
+++ src/main/java/com/awesoon/IlmsApplication.java
@@ -0,0 +1,12 @@
+package com.awesoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlmsApplication.class, args);
+	}
+}
